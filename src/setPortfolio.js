bd43bc3b3c3f4d3aa601ca536937576774123e14
bulbashist <nbulbashist@gmail.com>
2021-12-27 21:41:44 +0300

    feat: create portfolio section script

--- src/setPortfolio.js
+++ src/setPortfolio.js
@@ -1,18 +1,19 @@
 export default class Portfolio {
     constructor() {
-
+        this.seasons = ['winter', 'spring', 'summer', 'autumn'];
     }
 };
 
 Portfolio.prototype.changeImg = function(season) {
     const elems = document.querySelectorAll('.portfolio-photos__img');
     elems.forEach((elem, index) => {
-        elem.src = require(`../assets/portfolio/seasons/${season}/${index + 1}.jpg`);
+        const img = new Image();
+        img.src = require(`../assets/portfolio/${season}/${index + 1}.jpg`);
+        img.onload = () => elem.src = img.src;
     });
 }
 
 Portfolio.prototype.addListeners = function() {
-    const seasons = ['winter', 'spring', 'summer', 'autumn'];
     const elems = document.querySelectorAll('.portfolio-panel__btn');
     const goldColor = '#bdae82';
 
@@ -24,7 +25,7 @@ Portfolio.prototype.addListeners = function() {
             });
             elem.style.backgroundColor = goldColor;
             elem.style.color = 'black';
-            this.changeImg(seasons[index]);
+            this.changeImg(this.seasons[index]);
         });
     });
 }
