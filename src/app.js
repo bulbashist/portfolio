67d6f4cccf2a10488cdd663aa7f6ad754eb04db6
bulbashist <nbulbashist@gmail.com>
2021-12-28 13:50:22 +0300

    feat: add translation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import '../styles/style.scss';
 import Portfolio from './setPortfolio';
+import Translator from './translator';
 
 const portfolio = new Portfolio();
+new Translator();
 
 portfolio.addListeners();
